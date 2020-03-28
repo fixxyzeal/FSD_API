c1d67678cf0a0e1efcf90c7b8a610caad51174ef
Fixxy <kewell5@live.com>
2020-03-28 15:12:53 +0700

    fix error reference in BO project

--- BO/ViewModels/Auth.cs
+++ BO/ViewModels/Auth.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 using System.Text;
 
 namespace BO.ViewModels
@@ -10,6 +10,7 @@ namespace BO.ViewModels
         public string Email { get; set; }
 
         public string UserName { get; set; }
+
         public string Password { get; set; }
     }
 }
